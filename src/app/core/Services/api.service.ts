db3369af69139d5cc8d43ecc1f718ae9870cc6d1
MuhammadAsif521 <m.asif340315@example.com>
2025-09-14 22:55:49 +0500

    Update API service to use production environment configuration

--- src/app/core/Services/api.service.ts
+++ src/app/core/Services/api.service.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs'
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,4 @@
 export const environment = {
   production: true,
-  firebase: {
-  apiKey: "AIzaSyAPLEe-wcAhkflXej7t2xtFBQ5XFO24V0k",
-  authDomain: "portfolio-dc1dd.firebaseapp.com",
-  projectId: "portfolio-dc1dd",
-  storageBucket: "portfolio-dc1dd.firebasestorage.app",
-  messagingSenderId: "713875541582",
-  appId: "1:713875541582:web:9bad4c64e982e332cc7d1f",
-  measurementId: "G-0Y90WYLB57"
-  }
+  apiKey: 'https://portfolio-backend-dong.onrender.com/api',
 };
